874139307fde48cc01672a1fea50005fe814b228
Wilson Neto <wilson.neto@redesaoroque.com.br>
2024-11-06 16:37:18 -0300

    feat: added express-async-errors

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,7 +4,6 @@ const router = Router();
 
 router.get("/teste", (request: Request, response: Response) => {
   response.status(200).json({ nome: "Sujeito Pizza" });
-  return;
 });
 
 export { router };
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,26 @@
-import express, { Express } from "express";
+import express, { Express, NextFunction, Request, Response } from "express";
+import "express-async-errors";
+
 import { router } from "./routes";
 
 const app: Express = express();
 
 app.use(express.json());
 app.use(router);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof Error) {
+      response.status(400).json({
+        error: error.message,
+      });
+    }
+
+    response.status(500).json({
+      status: "error",
+      message: "Internal server error",
+    });
+  }
+);
 
 app.listen(3333, () => console.log("Servidor online"));
