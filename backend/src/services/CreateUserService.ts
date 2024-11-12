e225c429f1b257b8d28da3eea7051010195edbc3
Wilson Neto <wilson.neto@redesaoroque.com.br>
2024-11-12 16:48:00 -0300

    feat(user): created CreateUserService class

---
+++ backend/src/services/CreateUserService.ts
@@ -0,0 +1,42 @@
+import prismaClient from "../prisma";
+
+interface UserRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  async execute({ name, email, password }: UserRequest) {
+    if (!email) {
+      throw new Error("Email incorrect");
+    }
+
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    const user = await prismaClient.user.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      },
+    });
+
+    return user;
+  }
+}
+
+export { CreateUserService };
