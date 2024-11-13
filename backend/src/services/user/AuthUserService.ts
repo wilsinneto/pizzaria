c3a18362d11244e9b109cfe73a740df0130c7c8e
Wilson Neto <wilson.neto@redesaoroque.com.br>
2024-11-13 16:15:53 -0300

    feat(user): created AuthUserService class

---
+++ backend/src/services/user/AuthUserService.ts
@@ -0,0 +1,49 @@
+import { compare } from "bcryptjs";
+import prismaClient from "../../prisma";
+import { sign } from "jsonwebtoken";
+
+interface AuthRequest {
+  email: string;
+  password: string;
+}
+
+class AuthUserService {
+  async execute({ email, password }: AuthRequest) {
+    const user = await prismaClient.user.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      throw new Error("User/password incorrect");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("User/password incorrect");
+    }
+
+    const token = sign(
+      {
+        name: user.name,
+        email: user.email,
+      },
+      String(process.env.JWT_SECRET),
+      {
+        subject: user.id,
+        expiresIn: "30d",
+      }
+    );
+
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      token,
+    };
+  }
+}
+
+export { AuthUserService };
