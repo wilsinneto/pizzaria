c003444a57f946f777d23b761a6e0edc633023d4
Wilson Neto <wilson.neto@redesaoroque.com.br>
2024-11-14 16:31:22 -0300

    feat: created "/category" route

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,11 +3,17 @@ import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 
 const router = Router();
 
 router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
 router.get("/me", isAuthenticated, new DetailUserController().handle);
+router.post(
+  "/category",
+  isAuthenticated,
+  new CreateCategoryController().handle
+);
 
 export { router };
