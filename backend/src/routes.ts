d73122319f8d306a4bc09743e78e5ab96ba5b481
Wilson Neto <wilson.neto@redesaoroque.com.br>
2024-11-19 15:53:19 -0300

    feat(product): added product creation

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -5,17 +5,21 @@ import { DetailUserController } from "./controllers/user/DetailUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { ListaCategoryController } from "./controllers/category/ListaCategoryController";
+import { CreateProductController } from "./controllers/product/CreateProductController";
 
 const router = Router();
 
 router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
 router.get("/me", isAuthenticated, new DetailUserController().handle);
+
 router.post(
   "/category",
   isAuthenticated,
   new CreateCategoryController().handle
 );
 router.get("/category", isAuthenticated, new ListaCategoryController().handle);
+
+router.post("/product", isAuthenticated, new CreateProductController().handle);
 
 export { router };
---
+++ backend/src/services/product/CreateProductService.ts
@@ -0,0 +1,21 @@
+interface ProductRequest {
+  name: string;
+  price: string;
+  description: string;
+  banner: string;
+  category_id: string;
+}
+
+class CreateProductService {
+  async execute({
+    name,
+    price,
+    description,
+    banner,
+    category_id,
+  }: ProductRequest) {
+    return { ok: true };
+  }
+}
+
+export { CreateProductService };
