1bc81af410b1d5d03f9df68d1eb1c5c9e99e6b8c
Wilson Neto <wilson.neto@redesaoroque.com.br>
2024-12-03 11:33:09 -0300

    feat(user): saved token in cookie

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,8 +2,50 @@ import Image from "next/image";
 import styles from "./page.module.scss";
 import logoImg from "/public/logo.svg"
 import Link from "next/link";
+import { api } from "@/services/api";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
 
 export default function Page() {
+
+  async function handleLogin(formData: FormData) {
+    "use server"
+
+    const email = formData.get("email")
+    const password = formData.get("password")
+
+    if (!email || !password) {
+      return
+    }
+
+    try {
+      const response = await api.post("/session", {
+        email,
+        password
+      })
+      const { token } = response.data
+
+      if (!token) {
+        return
+      }
+
+      const expressTime = 60 * 60 * 24 * 30 * 1000;
+      const cookieStore = await cookies()
+
+      cookieStore.set("session", token, {
+        maxAge: expressTime,
+        path: "/",
+        httpOnly: false,
+        secure: process.env.NODE_ENV === "production"
+      })
+    } catch (error) {
+      console.log("error", error)
+      return
+    }
+
+    redirect("/dashboard")
+  }
+
   return(
     <>
       <div className={styles.containerCenter}>
@@ -13,7 +55,7 @@ export default function Page() {
         />
 
         <section className={styles.login}>
-          <form>
+          <form action={handleLogin}>
             <input
               type="email"
               required
