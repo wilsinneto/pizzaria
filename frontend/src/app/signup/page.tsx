a9b388b35619abf8439f8d6bbd12a158a55bd895
Wilson Neto <wilson.neto@redesaoroque.com.br>
2024-12-03 10:37:54 -0300

    feat(user): integrated login register with api

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -2,8 +2,36 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "../page.module.scss";
 import logoImg from "/public/logo.svg";
+import { api } from "@/services/api";
+import { redirect } from "next/navigation"
 
 export default function Signup() {
+
+  async function handleRegister(formData: FormData) {
+    "use server"
+
+    const name = formData.get("name")
+    const email = formData.get("email")
+    const password = formData.get("password")
+
+    if (!name || !email || !password) {
+      console.log('PREENCHA TODOS OS CAMPOS')
+      return
+    }
+
+    try {
+      await api.post("/users", {
+        name,
+        email,
+        password
+      })
+    } catch (error) {
+      console.log('error', error)
+    }
+
+    redirect("/")
+  }
+
   return(
     <>
       <div className={styles.containerCenter}>
@@ -14,7 +42,7 @@ export default function Signup() {
 
         <section className={styles.login}>
           <h1>Criando sua conta</h1>
-          <form>
+          <form action={handleRegister}>
             <input
               type="text"
               required
