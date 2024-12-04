4bfbfa9d35bc66f1a13c83abc972d06bded08b4e
Wilson Neto <wilson.neto@redesaoroque.com.br>
2024-12-04 15:35:17 -0300

    feat: added handleLogout function

--- frontend/src/app/dashboard/components/header/index.tsx
+++ frontend/src/app/dashboard/components/header/index.tsx
@@ -1,11 +1,23 @@
+"use client"
+
 import Link from "next/link"
 import Image from "next/image"
 import { LogOutIcon } from "lucide-react";
 import logoImg from "/public/logo.svg";
 
 import styles from "./styles.module.scss"
+import { deleteCookie } from "cookies-next";
+import { useRouter } from "next/navigation";
 
 export function Header() {
+  const router = useRouter();
+
+  async function handleLogout() {
+    deleteCookie("session", { path: "/" })
+
+    router.replace("/")
+  }
+
   return(
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
@@ -28,7 +40,7 @@ export function Header() {
             Produto
           </Link>
 
-          <form>
+          <form action={handleLogout}>
             <button type="submit">
               <LogOutIcon size={24} color="#FFF" />
             </button>
