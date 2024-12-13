3f4557f8a59f4adaad1f1ed0e06fd6795d7ed5a1
Wilson Neto <wilson.neto@redesaoroque.com.br>
2024-12-13 15:53:56 -0300

    feat: setuped sonner in root

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.scss";
+import { Toaster } from "sonner";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,6 +27,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Toaster
+          position="bottom-right"
+          toastOptions={{
+            style: {
+              backgroundColor: "#f1f1f1",
+              color: "#131313",
+              borderColor: "rgba(255,255,255, 0.5)"
+            }
+          }}
+        />
         {children}
       </body>
     </html>
