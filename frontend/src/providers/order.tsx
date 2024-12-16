4105714740a1738c6437dee535f29f253b074751
Wilson Neto <wilson.neto@redesaoroque.com.br>
2024-12-16 16:33:17 -0300

    feat: added request by order id in onRequestOpen function

--- frontend/src/providers/order.tsx
+++ frontend/src/providers/order.tsx
@@ -1,11 +1,37 @@
 "use client"
 
 import { createContext, ReactNode, useState } from "react";
+import { api } from "@/services/api";
+import { getCookieClient } from "@/lib/CookieClient";
+
+interface OrderItemProps {
+  id: string;
+  amount: number;
+  created_at: Date;
+  order_id: string;
+  product_id: string;
+  order: {
+    id: string;
+    table: number;
+    status: boolean;
+    draft: boolean;
+    name: string | null;
+  },
+  product: {
+    id: string;
+    name: string;
+    price: string;
+    description: string;
+    banner: string;
+    category_id: string;
+  }
+}
 
 type OrderContextData = {
   isOpen: boolean;
-  onRequestOpen: () => void;
+  onRequestOpen: (order_id: string) => Promise<void>;
   onRequestClose: () => void;
+  order: OrderItemProps[];
 }
 
 type OrderProviderProps = {
@@ -16,8 +42,21 @@ export const OrderContext = createContext({} as OrderContextData)
 
 export function OrderProvider({ children }: OrderProviderProps) {
   const [isOpen, setIsOpen] = useState(false);
+  const [order, setOrder] = useState<OrderItemProps[]>([])
 
-  function onRequestOpen() {
+  async function onRequestOpen(order_id: string) {
+    const token = await getCookieClient()
+
+    const response = await api.get("/order/detail", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      params: {
+        order_id
+      }
+    })
+
+    setOrder(response.data)
     setIsOpen(true);
   }
 
@@ -30,7 +69,8 @@ export function OrderProvider({ children }: OrderProviderProps) {
       value={{
         isOpen,
         onRequestOpen,
-        onRequestClose
+        onRequestClose,
+        order
       }}
     >
       {children}
