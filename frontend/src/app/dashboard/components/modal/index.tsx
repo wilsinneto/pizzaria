225ccfde970dd6bf223758b1d90ae51664d3a6e3
Wilson Neto <wilson.neto@redesaoroque.com.br>
2024-12-16 16:34:14 -0300

    feat: created ModalOrder component

---
+++ frontend/src/app/dashboard/components/modal/index.tsx
@@ -0,0 +1,44 @@
+import { use } from "react"
+import { X } from "lucide-react";
+
+import { OrderContext } from "@/providers/order";
+import styles from "./styles.module.scss";
+
+export function ModalOrder() {
+  const { onRequestClose, order } = use(OrderContext)
+
+  return(
+    <dialog className={styles.dialogContainer}>
+      <section className={styles.dialogContent}>
+        <button className={styles.dialogBack} onClick={onRequestClose}>
+          <X size={40} color="#FF3f4b" />
+        </button>
+
+        <article className={styles.container}>
+          <h2>Detalhes do pedido</h2>
+
+          <span className={styles.table}>
+            Mesa <b>{order[0].order.table}</b>
+          </span>
+
+          { order[0].order?.name && (
+            <span className={styles.name}>
+              <b>{order[0].order.name}</b>
+            </span>
+          )}
+
+          {order.map(item => (
+            <section className={styles.item} key={item.id}>
+              <span>{item.amount} - <b>{item.product.name}</b></span>
+              <span className={styles.description}>{item.product.description}</span>
+            </section>
+          ))}
+
+          <button className={styles.buttonOrder}>
+            Concluir pedido
+          </button>
+        </article>
+      </section>
+    </dialog>
+  );
+}
