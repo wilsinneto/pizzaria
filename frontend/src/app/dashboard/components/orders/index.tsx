e649b1cbac6a1820d902e22ef4b3cb6e1f1b0c08
Wilson Neto <wilson.neto@redesaoroque.com.br>
2024-12-17 09:27:45 -0300

    feat: added handleRefresh function and message for empty list

--- frontend/src/app/dashboard/components/orders/index.tsx
+++ frontend/src/app/dashboard/components/orders/index.tsx
@@ -2,6 +2,8 @@
 
 import { use } from "react";
 import { RefreshCw } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 import { OrderProps } from "@/lib/order.type";
 import { ModalOrder } from "@/app/dashboard/components/modal";
@@ -14,9 +16,15 @@ interface Props {
 
 export function Orders({ orders }: Props) {
   const { isOpen, onRequestOpen } = use(OrderContext)
+  const router = useRouter()
 
   async function handleDetailOrder(orderId: string) {
     await onRequestOpen(orderId)
+  }
+
+  function handleRefresh() {
+    router.refresh()
+    toast.success("Pedidos atualizados com sucesso!")
   }
 
   return(
@@ -25,24 +33,28 @@ export function Orders({ orders }: Props) {
         <section className={styles.containerHeader}>
           <h1>Últimos pedidos</h1>
 
-          <button>
+          <button onClick={handleRefresh}>
             <RefreshCw size={24} color="#3fffa3" />
           </button>
         </section>
 
         <section className={styles.listOrders}>
-          {
-            orders.map(order => (
-              <button
-                className={styles.orderItem}
-                key={order.id}
-                onClick={() => handleDetailOrder(order.id)}
-              >
-                <div className={styles.tag} />
-                <span>Mesa {order.table}</span>
-              </button>
-            ))
-          }
+          {!orders?.length && (
+            <span className={styles.emptyItem}>
+              Nenhum pedido aberto no momento...
+            </span>
+          )}
+
+          {orders.map(order => (
+            <button
+              className={styles.orderItem}
+              key={order.id}
+              onClick={() => handleDetailOrder(order.id)}
+            >
+              <div className={styles.tag} />
+              <span>Mesa {order.table}</span>
+            </button>
+          ))}
         </section>
       </main>
 
