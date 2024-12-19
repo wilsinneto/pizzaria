05da4949f7f0ae9c622dd6ca9018e6e7c3eba895
Wilson Neto <wilson.neto@redesaoroque.com.br>
2024-12-19 11:07:19 -0300

    feat: setup Routes in App

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,22 +1,19 @@
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from "./src/routes";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <NavigationContainer>
       <StatusBar
         backgroundColor="#1d1d2e"
         barStyle="light-content"
         translucent={false}
       />
-    </View>
+
+      <Routes />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
