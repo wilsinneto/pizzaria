7b5e74794bada303a4a720711c4afaee2cf7b77f
Wilson Neto <wilson.neto@redesaoroque.com.br>
2024-12-23 12:03:27 -0300

    feat(dashboard): created page and added styled

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -7,7 +7,11 @@ const Stack = createNativeStackNavigator()
 function AppRoutes() {
   return(
     <Stack.Navigator>
-      <Stack.Screen name="Dashboard" component={Dashboard}/>
+      <Stack.Screen
+        name="Dashboard"
+        component={Dashboard}
+        options={{ headerShown: false }}
+      />
     </Stack.Navigator>
   )
 }
